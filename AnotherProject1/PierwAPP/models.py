7e04542c288488e69e41cef066a11c723b8997c7
Gerard <gerard.dziwinski@infotech.edu.pl>
2024-10-16 13:51:32 +0200

    Wszystko

--- AnotherProject1/PierwAPP/models.py
+++ AnotherProject1/PierwAPP/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Skelp(models.Model):
+    klucz = models.FloatField()
+
+class Chusteczki(models.Model):
+    nazwa = models.CharField(max_length=120)
+    ilosc = models.IntegerField()
+    lokacja = models.ForeignKey(Skelp, on_delete=models.CASCADE)
--- AnotherProject1/PierwAPP/views.py
+++ AnotherProject1/PierwAPP/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Chusteczki
 # Create your views here.
+def main(request):
+    Model_Data = Chusteczki.objects.all()
+    return render(request, 'index.html', context={
+        "HTML_DATA": Model_Data
+    })
